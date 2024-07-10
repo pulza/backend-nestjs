739aa68e67cb545da5d7d2927336c2e90f7b17cd
Jiyu Jang <apryll7995@gmail.com>
2024-07-10 22:35:52 +0900

    feat: 필요없는 global provider import 제거

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, ConfigService],
+  providers: [AuthService],
 })
 export class AuthModule {}
