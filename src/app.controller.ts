ca45d4e234fe29e41f8aa6d3343008692dc6c240
Jiyu Jang <apryll7995@gmail.com>
2024-07-11 09:42:40 +0900

    feat: health check를 위한 endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Roles } from './common/decorator/roles.decorator';
 
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get()
+  @Roles('public')
   getHello(): string {
     return 'Hello World!';
   }
